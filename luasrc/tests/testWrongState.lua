c2469e464e6114a30b866b6a4c8acbcd5d80fe38
Julien Cornebise <jucor@google.com>
2014-07-01 11:34:10 +0100

    Skip pre-loading test when using totem-run
    The test of behaviour without having loaded randomkit must
    be run separately: when using totem-run, randomkit has already
    been loaded, making it impossible to test the behaviour
    when randomkit is not yet loaded.

--- luasrc/tests/testWrongState.lua
+++ luasrc/tests/testWrongState.lua
@@ -4,6 +4,10 @@ local seedTest = {}
 local tester = totem.Tester()
 
 function seedTest.testStateBeforeRequire()
+    if randomkit then
+        print('Randomkit already loaded, skipping test, please run individually')
+        return
+    end
     local state = torch.getRNGState()
     require 'randomkit'
     tester:assertError(function() torch.setRNGState(state) end, 'Failed to generate error')
